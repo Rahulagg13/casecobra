4a862d33ecfe763d5e46fcd047c33192a6ff523c
Rahul Aggarwal <rahulaggarwal@Rahuls-MacBook-Air.local>
2024-10-06 17:30:18 +0530

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "my-blob-store.public.blob.vercel-storage.com",
-        port: "",
-      },
-    ],
+    domains: ["utfs.io"],
   },
   compiler: {
     removeConsole: false,
